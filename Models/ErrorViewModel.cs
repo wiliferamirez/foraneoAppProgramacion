d53a72d94c2fbe9a414498aa248a6e075e6d146e
William Ramirez_Pc <wframirez98@gmail.com>
2024-10-26 14:55:06 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace foraneoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
